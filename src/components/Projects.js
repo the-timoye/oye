8ca658bf79317e4a9101121c6dbabcfda3c07ef7
the-timoye <thetimoye@gmail.com>
2021-05-21 20:04:43 +0100

    feat(projects): add milola project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,14 @@ import aggsoga from "../assets/images/portfolio/aggsoga.png";
 import bondhair from "../assets/images/portfolio/bondhair.png";
 import enaro from "../assets/images/portfolio/enaro.png";
 import indigo from "../assets/images/indigo.png";
+import milola from "../assets/images/milola.png";
 
 export const Projects = () => {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         setProjects([
+            { thumbnail: milola, title: 'Milola', description: 'A simple website/portfolio', tools: 'ReactJs, SCSS', link: 'https://milola.netlify.app' },
             { thumbnail: enaro, title: 'Enaro', description: 'Collaborating with other devs, I built the "Buy Units" section of the Enaro Web-app - an app that manages estate users meters.', tools: 'ReactJs,NodeJs', link: 'private' },
             { thumbnail: fesbach, title: 'FESBACH Schools', description: 'A website for a Nigerian Secondary School providing a glimpse of the school to prospective students.', tools: 'Wordpress', link: 'http://www.fesbachschools.com' },
             { thumbnail: indigo, title: 'Indigo', description: 'Built the doctors scheduling/calender, and tele-conferencing sections in the Indigo EMR.', tools: 'Laravel, JQuery', link: 'https://indigo.summitech.ng' },
@@ -33,7 +35,7 @@ export const Projects = () => {
                 {project.link !== 'private' && (
                     <div className='text-center mt-5'>
                         <a className='link __dark' href={project.link} target='_blank' rel='noreferrer'> Visit &rarr;
-                    <hr className="link__hr __dark" /> </a>
+                            <hr className="link__hr __dark" /> </a>
 
                     </div>
                 )}
@@ -45,8 +47,11 @@ export const Projects = () => {
             {projects.map((project, index) => {
                 return (
                     <div className='project-container'>
+
                         <div className='project-container__inner'>
+
                             <div className="project-thumbnail" key={index}>
+                                <div className='project-container__inner __overlay'> {project.title} </div>
                                 <img src={project.thumbnail} alt={`${project.thumbnail}-${index}`} />
                             </div>
                             {displayDetails(project, index)}
